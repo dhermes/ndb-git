7aacdc952b97ba96e30668cc45b14336238bd828
Danny Hermes <daniel.j.hermes@gmail.com>
2015-01-12 00:31:04 -0800

    Using Travis cache and setting GAE environment variable.

---
+++ get_app_engine_sdk.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+[[ -d ${HOME}/cache ]] || mkdir ${HOME}/cache
+cd ${HOME}/cache
+
+if [[ ! -f google_appengine_1.9.17.zip ]]; then
+  wget https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.17.zip -nv
+fi
+unzip -q google_appengine_1.9.17.zip
+mv google_appengine/ ..
